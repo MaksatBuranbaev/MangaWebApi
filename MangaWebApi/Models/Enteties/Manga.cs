b107cdf62edb372fc4bae832e9d8ba30d095f937
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-08-20 13:52:50 +0600

    add manga model

---
+++ MangaWebApi/Models/Enteties/Manga.cs
@@ -0,0 +1,10 @@
+﻿namespace MangaWebApi.Models.Enteties
+{
+    public class Manga
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}

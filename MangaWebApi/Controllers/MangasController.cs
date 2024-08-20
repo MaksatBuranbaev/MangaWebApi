c65fe93d2af50d633b9792d1cd926255bfb2db58
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-08-20 14:51:08 +0600

    add controllers

---
+++ MangaWebApi/Controllers/MangasController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MangaWebApi.Data;
+using MangaWebApi.Models.Enteties;
+
+namespace MangaWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MangasController : ControllerBase
+    {
+        private readonly MangaDbContext _context;
+
+        public MangasController(MangaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Manga>>> GetMangas()
+        {
+            return await _context.Mangas.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Manga>> GetManga(Guid id)
+        {
+            var manga = await _context.Mangas.FindAsync(id);
+
+            if (manga == null)
+            {
+                return NotFound();
+            }
+
+            return manga;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutManga(Guid id, Manga manga)
+        {
+            if (id != manga.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(manga).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MangaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Manga>> PostManga(Manga manga)
+        {
+            _context.Mangas.Add(manga);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetManga", new { id = manga.Id }, manga);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteManga(Guid id)
+        {
+            var manga = await _context.Mangas.FindAsync(id);
+            if (manga == null)
+            {
+                return NotFound();
+            }
+
+            _context.Mangas.Remove(manga);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MangaExists(Guid id)
+        {
+            return _context.Mangas.Any(e => e.Id == id);
+        }
+    }
+}

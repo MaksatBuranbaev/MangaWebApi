a5821c84df427c31eb111887fa065b307172db9e
Maksat Buranbaev <zenfonemaxprom124@gmail.com>
2024-08-20 13:57:59 +0600

    create DbContext

---
+++ MangaWebApi/Data/MangaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MangaWebApi.Models.Enteties;
+using Microsoft.EntityFrameworkCore;
+
+namespace MangaWebApi.Data
+{
+    public class MangaDbContext: DbContext
+    {
+        public MangaDbContext(DbContextOptions<MangaDbContext> options): base(options) {}
+
+        public DbSet<Manga> Mangas { get; set; }
+    }
+}
